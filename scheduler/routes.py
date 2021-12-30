49146e9c98463bf79f3e4d44277c2d7f973e5f8b
anmaxwell <aniamaxwell@gmail.com>
2021-12-30 14:21:53 +0000

    updated run model page with validation

--- scheduler/routes.py
+++ scheduler/routes.py
@@ -3,26 +3,26 @@ from flask import render_template, flash
 from scheduler import app
 from scheduler.forms import RunModel
 
-@app.route('/') #Page to display tiles
+@app.route('/') #Home page
 def home():
 
     return render_template('home.html')
 
-@app.route('/train') #Page to display tiles
+@app.route('/train') #Page to allow you to train model
 def train_model():
 
     #form = SetKbase()
 
     return render_template('train_model.html')
 
-@app.route('/run') #Page to display tiles
+@app.route('/run', methods=['GET', 'POST']) #Page to allow you to run model
 def run_model():
 
     form = RunModel()
 
     return render_template('run_model.html',title='RunModel', form=form)
 
-@app.route('/data') #Page to display tiles
+@app.route('/data') #Page to manage data
 def data():
 
     #form = SetKbase()
