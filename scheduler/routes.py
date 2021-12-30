b0e877ff4b1762e4850315a657afc736ac549762
anmaxwell <aniamaxwell@gmail.com>
2021-12-30 13:27:37 +0000

    started UI for project

--- scheduler/routes.py
+++ scheduler/routes.py
@@ -6,3 +6,24 @@ from scheduler import app
 def home():
 
     return render_template('home.html')
+
+@app.route('/train') #Page to display tiles
+def train_model():
+
+    #form = SetKbase()
+
+    return render_template('train_model.html')
+
+@app.route('/run') #Page to display tiles
+def run_model():
+
+    #form = SetKbase()
+
+    return render_template('run_model.html')
+
+@app.route('/data') #Page to display tiles
+def data():
+
+    #form = SetKbase()
+
+    return render_template('data.html')
